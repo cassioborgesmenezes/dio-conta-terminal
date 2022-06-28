1e87b855543d8581a3425954eed91d996c1c63b4
Cassio MENEZES <cassio.menezes@outlook.com.br>
2022-06-28 10:31:14 -0300

    fix: rebuild package

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,10 +1,5 @@
-import tech.cbm.dio.banco.ContaTerminalController;
-
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        ContaTerminalController controller = new ContaTerminalController();
-
-		controller.getDadosFromTerminal();
-		controller.printMessage();
+        
     }
 }
