11785a2c280fa6f8f3a9f5a058ba628816b2a630
Cassio MENEZES <cassio.menezes@outlook.com.br>
2022-06-28 10:15:42 -0300

    feat: implementar cliente

--- ContaBanco/src/tech/cbm/dio/banco/Cliente.java
+++ ContaBanco/src/tech/cbm/dio/banco/Cliente.java
@@ -1,5 +1,33 @@
 package tech.cbm.dio.banco;
 
 public class Cliente {
+	private String					nome;
+	private String					sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getNomeCompleto() {
+		return String.format("%s %s", nome, sobrenome);
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + ", sobrenome=" + sobrenome + "]";
+	}
+
 }
